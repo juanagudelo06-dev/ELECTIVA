f156006909fcc403317c4199d323a83637bf75f5
juanagudelo06-dev <juan.agudelo06@uptc.edu.co>
2026-08-28 13:12:33 -0500

    Add files via upload

---
+++ 02_datos_variables.py
@@ -0,0 +1,28 @@
+# nombre=(input("digite su nombre por favor "))
+# edad=int(input("digite su edad "))
+# temp_corporal=float(input("digite su temperatura actual "))
+# nota=float(input("digite su nota de su capacitacion "))
+# carnet=(input("digite si usted posee carnet o no "))
+# mayor_edad= edad>=18
+# temp_adecuada= temp_corporal <=37.5
+# cap_aprobada= nota>=3
+# tiene_carnet=carnet=='si'
+# print((nombre))
+# print('mayor_edad',mayor_edad)
+# print('temp_adecuada',temp_adecuada)
+# print('cap_aprobada',cap_aprobada)
+# print('tiene_carnet',tiene_carnet)
+# cumple_requistos= mayor_edad and temp_adecuada and cap_aprobada and tiene_carnet
+# print('cumple_requistos',cumple_requistos)
+
+nombre=(input("Digite su nombre por favor "))
+producto=(input("Digite el producto "))
+precio=float(input("Digite el precio unitario "))
+cantidad=int(input("Digite la cantidad "))
+total=precio*cantidad
+
+print('Nombre: ', nombre)
+print('Producto: ', producto)
+print('Precio: ', precio)
+print('Cantidad: ', cantidad)
+print('Total: ', total)
